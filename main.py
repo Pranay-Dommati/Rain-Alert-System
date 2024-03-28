18035307e145ebca75aeb12fa6564f6e61dc9997
Pranay-Dommati <153319504+Pranay-Dommati@users.noreply.github.com>
2024-03-28 23:23:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests
 from twilio.rest import Client
 import os
-API_KEY = "76ed351edfc8000f97078f1f4c419ee2"
-CITY = "Hyderabad,Telangana"
+API_KEY = YOUR_APIKEY
+CITY = "Hyderabad, Telangana"
 parameters = {
     "q":CITY,
     "appid":API_KEY,
     "cnt":4
 }
 
-account_sid = 'AC3eeb1bbcd63b613dc89ce26cbf9cda10'
-auth_token = '13ef7044edbc12b3f61aee2e3c925f14'
+account_sid = YOUR_ACCOUNT_SID
+auth_token = YOUR_AUTH_TOKEN
 client = Client(account_sid, auth_token)
 
 response = requests.get(url="http://api.openweathermap.org/data/2.5/forecast",
@@ -23,7 +23,7 @@ for weather_code in weather_codes:
     if weather_code < 700:
         message = client.messages.create(
             body="It's rainy day, don't forgot to bring an ☔",
-            from_='+16107560774',
-            to='+917569897932'
+            from_= TWILIO_MOBILE_NUMBER,
+            to= TO_MOBILE_NUMBER
         )
         break
